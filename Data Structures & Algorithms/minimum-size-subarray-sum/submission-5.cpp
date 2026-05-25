80473a5f212c8a5e1caef5527f037d53f667efec
beka betsunaidze <b00147423@mytudublin.ie>
2026-05-25 18:55:12 +0100

    Add: minimum-size-subarray-sum - submission-5

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-5.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+    
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> prefix(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+            cout << "\nprefix[i] = " << prefix[i];
+        }
+
+        int left = 0;
+        int ans = INT_MAX;
+
+        for (int right = 1; right <= n; right++) {
+
+            int need = prefix[right] - target;
+
+            while (left < right && prefix[left] <= need) {
+
+                ans = min(ans, right - left);
+
+                left++;
+            }
+        }
+
+        if (ans == INT_MAX) {
+            return 0;
+        } else {
+            return ans;
+        }
+    }
+};
